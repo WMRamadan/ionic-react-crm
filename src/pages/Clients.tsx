38911aa450c5a78d141c671322e2bd186f6e0e09
WMRamadan <thebigguyis@gmail.com>
2020-08-30 22:55:11 +0300

    add client cards

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { person } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Clients.css';
 
@@ -11,13 +12,31 @@ const Clients: React.FC = () => {
           <IonTitle>Clients</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Clients</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Clients page" />
+      <ExploreContainer name="Clients page" />
+      <IonContent>
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={person} slot="start" />
+            <IonLabel>Client Name</IonLabel>
+            <IonButton fill="outline" slot="end">Details</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            Last service: August 15, 1981
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={person} slot="start" />
+            <IonLabel>Client Name</IonLabel>
+            <IonButton fill="outline" slot="end">Details</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            Last service: August 15, 1981
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
